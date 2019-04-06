a05402198a175217760b3b733fe590e78590798d
庭嘉 張 <52713rose@gmail.com>
2019-04-06 12:56:58 +0800

    HiHi

---
+++ Homework2/Homework2/Models/OpenData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework2.Models
+{
+    class OpenData
+    {
+        public string Date { get; set; }
+        public string ReservoirName { get; set; }
+        public string WaterLevel { get; set; }
+        public string WaterStorageRate { get; set; }
+    }
+}
--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Homework2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace Homework2
 {
@@ -7,15 +11,63 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
-            XmlTextReader reader = new XmlTextReader("water.xml");
-
-            while (reader.Read())
-            {
-                Console.WriteLine(reader.Name);
-            }
-            Console.ReadLine();
-
+            var nodes = findOpenData();
+            showOpenData(nodes);
+            Console.ReadKey();
             //Console.WriteLine("Hello World!");
+        }
+
+        static List<OpenData> findOpenData()
+        {
+            List<OpenData> result = new List<OpenData>();
+
+            var xml = XElement.Load(@"App_Data\water.xml.xml");
+            //var nodes = xml.Descendants("LiyutanReservoirDailyWaterInformation").ToList();
+            List<XElement> nodes = xml.Descendants("LiyutanReservoirDailyWaterInformation").ToList();
+
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+
+                string string1 = node.Element("Date")?.Value?.Trim();
+                string string2 = node.Element("ReservoirName")?.Value?.Trim();
+                string string3 = node.Element("WaterLevel")?.Value?.Trim();
+                string string4 = node.Element("WaterStorageRate")?.Value?.Trim();
+
+                OpenData item = new OpenData();
+
+                item.Date = getValue(node, "Date");
+                item.ReservoirName = getValue(node, "ReservoirName");
+                item.WaterLevel = getValue(node, "WaterLevel");
+                item.WaterStorageRate = getValue(node, "WaterStorageRate");
+
+                result.Add(item);
+            }
+
+            private static string getValue(XElement node, string propertyName)
+            {
+                return node.Element(propertyName)?.Value?.Trim();
+            }
+
+            private static void showOpenData(List<OpenData> nodes)
+            {
+                Console.WriteLine(string.Format("共收到{0}筆的資料", nodes.Count));
+                //nodes.GroupBy(node => node.分類).ToList()
+                //    .ForEach(group =>
+                //    {
+                //        var key = group.Key;
+                //        var groupDatas = group.ToList();
+                //        var message = $"分類:{key},共有{groupDatas.Count()}筆資料";
+                //        Console.WriteLine(message);
+                //    });
+                nodes.ToList()
+                    .ForEach(node =>
+                    {
+                        var message = $"服務:{ node.ReservoirName}";
+                        Console.WriteLine(message);
+                    });
+                Console.WriteLine($"共:{nodes.Count}筆");
+            }
         }
 
         
